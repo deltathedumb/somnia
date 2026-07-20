79f9940f44564f6da7d51e89e11937e330c456db
deltathedumb <133447617+deltathedumb@users.noreply.github.com>
2026-07-20 14:40:03 -0500

    Expose unified Somnia object model

---
+++ somnia/model/__init__.py
@@ -0,0 +1,45 @@
+"""Unified Somnia object hierarchy and reflection API."""
+
+from .core import (
+    DataModel,
+    Folder,
+    Model,
+    ModelNode,
+    OBJECT_TYPES,
+    ObjectRegistry,
+    Property,
+    Service,
+    Signal,
+    SomniaObject,
+    UnknownModelNode,
+    UnknownObject,
+    register_object_class,
+    serialize_value,
+)
+
+# Import built-in object classes for registration side effects.
+from .native import NativeFunction, NativeLibrary, NativeLibraryService
+from .scripting import PortaPyRuntime, PythonScript, ScriptService
+
+__all__ = [
+    "DataModel",
+    "Folder",
+    "Model",
+    "ModelNode",
+    "NativeFunction",
+    "NativeLibrary",
+    "NativeLibraryService",
+    "OBJECT_TYPES",
+    "ObjectRegistry",
+    "PortaPyRuntime",
+    "Property",
+    "PythonScript",
+    "ScriptService",
+    "Service",
+    "Signal",
+    "SomniaObject",
+    "UnknownModelNode",
+    "UnknownObject",
+    "register_object_class",
+    "serialize_value",
+]
