b972b31d29d6eae1bc2ea989a113f0b0ba0e4e9c
deltathedumb <133447617+deltathedumb@users.noreply.github.com>
2026-07-20 16:59:55 -0500

    Expose raylib rendering backend

--- somnia/rendering/__init__.py
+++ somnia/rendering/__init__.py
@@ -1,12 +1,31 @@
 """Somnia rendering backend interfaces."""
 
-from .base import RenderFrame, Renderer, collect_mesh_objects, find_active_camera
+from .base import (
+    RenderFrame,
+    Renderer,
+    build_render_frame,
+    collect_mesh_objects,
+    find_active_camera,
+)
 from .null import NullRenderer
+from .raylib import (
+    RAYLIB_BRIDGE_NAME,
+    RaylibRenderer,
+    RecordingRaylibBridge,
+    create_raylib_library,
+    ensure_raylib_library,
+)
 
 __all__ = [
     "NullRenderer",
+    "RAYLIB_BRIDGE_NAME",
+    "RaylibRenderer",
+    "RecordingRaylibBridge",
     "RenderFrame",
     "Renderer",
+    "build_render_frame",
     "collect_mesh_objects",
+    "create_raylib_library",
+    "ensure_raylib_library",
     "find_active_camera",
 ]
