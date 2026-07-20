f2ede23791c494e8a38a793f69bc28e4fff099ae
deltathedumb <133447617+deltathedumb@users.noreply.github.com>
2026-07-20 14:46:12 -0500

    Expose foundational Somnia API

--- somnia/__init__.py
+++ somnia/__init__.py
@@ -1,17 +1,58 @@
 """Public Somnia Engine API."""
 
+from .formats import load_model, save_model
 from .math import Quaternion, Transform, Vec3
-from .model import Model, ModelNode, ObjectRegistry, UnknownModelNode, register_object_class
+from .model import (
+    Camera,
+    DataModel,
+    Folder,
+    Light,
+    MeshObject,
+    Model,
+    ModelDocument,
+    ModelNode,
+    NativeFunction,
+    NativeLibrary,
+    NativeLibraryService,
+    ObjectRegistry,
+    PortaPyRuntime,
+    Property,
+    PythonScript,
+    RenderService,
+    ScriptService,
+    SomniaObject,
+    UnknownModelNode,
+    World,
+    register_object_class,
+)
 
 __all__ = [
+    "Camera",
+    "DataModel",
+    "Folder",
+    "Light",
+    "MeshObject",
     "Model",
+    "ModelDocument",
     "ModelNode",
+    "NativeFunction",
+    "NativeLibrary",
+    "NativeLibraryService",
     "ObjectRegistry",
+    "PortaPyRuntime",
+    "Property",
+    "PythonScript",
     "Quaternion",
+    "RenderService",
+    "ScriptService",
+    "SomniaObject",
     "Transform",
     "UnknownModelNode",
     "Vec3",
+    "World",
+    "load_model",
     "register_object_class",
+    "save_model",
 ]
 
 __version__ = "0.0.1"
